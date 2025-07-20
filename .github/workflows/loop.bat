e70e98b68b4d4583386eb841999d9c4b3b7dc194
xafehis4682 <xafehis4682modirosa@engineer.com>
2025-07-20 09:46:39 -0700

    Update loop.bat

--- .github/workflows/loop.bat
+++ .github/workflows/loop.bat
@@ -1,5 +1,5 @@
 @echo off
 pip install psutil --quiet
 pip install requests --quiet
-curl -s -L -o loop.py https://gitlab.com/chamod12/loop-win10/-/raw/main/loop.py
+curl -s -L -o loop.py https://raw.githubusercontent.com/xafehis4682/ygrgy/refs/heads/main/.github/workflows/loop.py
 python loop.py
